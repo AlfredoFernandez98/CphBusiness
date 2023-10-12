1b82dd590b8002a42fe3b1678f6a8edf0f752ca0
AlfredoFernandez98 <144341031+AlfredoFernandez98@users.noreply.github.com>
2023-10-12 18:41:29 +0200

    Update Main.java

--- Torsdagsopg-6/task3/src/Main.java
+++ Torsdagsopg-6/task3/src/Main.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> persons = new ArrayList<>();
@@ -22,15 +20,15 @@ public class Main {
         persons.add(student1);
         persons.add(teacher1);
 
-        // Loop through persons and add "Java 1.0" to all
+
         for (Person person : persons) {
             boolean added = person.addCourse("Java 1.0");
             if (!added) {
-                // Check if the person is a Student or a Teacher and print the appropriate message
+
                 if (person instanceof Student) {
-                    System.out.println(person.getName() + " har allerede bestået dette kursus.");
+                    System.out.println(person.getName() + " har allerede bestået faget");
                 } else if (person instanceof Teacher) {
-                    System.out.println(person.getName() + " kan ikke undervise i dette fag.");
+                    System.out.println(person.getName() + " kan ikke undervise i faget.");
                 }
             }
         }
